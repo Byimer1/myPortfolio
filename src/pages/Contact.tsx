7c2da61dd57c176a70260a72e4f41502683106b9
Biruk Yimer <65928912+Byimer1@users.noreply.github.com>
2025-04-08 15:06:24 -0400

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -56,7 +56,6 @@ export const Contact = () => {
             </div>
 
             <a
-
               className="inline-flex items-center gap-2 bg-gray-800 hover:bg-gray-700 
                        text-white px-4 py-2 rounded-lg transition-colors duration-300"
             >
